6541df0d566b2c2064a345b6fa09a4a38ff97ed7
JavierUlisesAmarilla And <amarillajavierulises@gmail.com>
2023-05-29 23:44:24 -0500

    prepare edge texturing

--- src/App.vue
+++ src/App.vue
@@ -45,10 +45,9 @@ onMounted(() => {
   getModelMesh('models/model.stl', 'textures/cherry.jpg', 'textures/overlay.svg').then((modelMesh) => {
     modelMesh.rotation.x = -Math.PI / 2
     modelMesh.position.set(-6, 0, 3)
+    modelMesh.add(new THREE.AxesHelper(10))
     scene.add(modelMesh)
   })
-
-  // scene.add(new THREE.AxesHelper(10))
 })
 
 
@@ -114,7 +113,7 @@ async function getModelMesh(modelPath: string, texturePath: string, svgPath: str
         gl_FragColor = lightColor * col3 * nDotL;
       }
     `,
-    // wireframe: true,
+    wireframe: true,
   })
 
   // console.log('stlGeo: ', stlGeo)
@@ -160,11 +159,25 @@ function getStlMesh(geometry: any, material: any) {
   const faces = rawGeometry.faces
   // console.log('faces: ', faces)
   rawGeometry.faceVertexUvs[0] = []
+  let frontCnt = 0
+  let backCnt = 0
+  let leftCnt = 0
+  let rightCnt = 0
 
   for (let i = 0; i < faces.length; i++) {
     const v1 = rawGeometry.vertices[faces[i].a],
       v2 = rawGeometry.vertices[faces[i].b],
       v3 = rawGeometry.vertices[faces[i].c]
+    if (v1.y === min.y && v2.y === min.y && v3.y === min.y) {
+      frontCnt++
+    } else if (v1.y === max.y && v2.y === max.y && v3.y === max.y) {
+      backCnt++
+    } else if (v1.x === min.x && v2.x === min.x && v3.x === min.x) {
+      leftCnt++
+    } else if (v1.x === max.x && v2.x === max.x && v3.x === max.x) {
+      rightCnt++
+    } else {
+    }
     rawGeometry.faceVertexUvs[0].push([
       new THREE.Vector2((v1.x + offset.x) / range.x, (v1.y + offset.y) / range.y),
       new THREE.Vector2((v2.x + offset.x) / range.x, (v2.y + offset.y) / range.y),
@@ -172,6 +185,10 @@ function getStlMesh(geometry: any, material: any) {
     ])
   }
 
+  console.log('frontCnt: ', frontCnt)
+  console.log('backCnt: ', backCnt)
+  console.log('leftCnt: ', leftCnt)
+  console.log('rightCnt: ', rightCnt)
   rawGeometry.uvsNeedUpdate = true
 
   const stlMesh = new THREE.Mesh(rawGeometry, material)
